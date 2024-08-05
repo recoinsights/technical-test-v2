a991f40a750140f1f25f06d92ac3dee20122ec04
Wai Hein <whein@recoinsights.com>
2024-08-05 10:47:37 +0100

    template for tests

---
+++ backend/functions/test/createSurvey.test.ts
@@ -0,0 +1,43 @@
+import { getMongoService } from 'src/services/mongoService';
+import { getNotificationService } from 'src/services/notificationService';
+import { getSurveyService } from 'src/services/surveyService';
+import {
+  describe,
+  it,
+  expect,
+  vi,
+  afterEach,
+  beforeAll,
+  afterAll
+} from 'vitest';
+import { createOne as createSurveyHandler } from '../src/handlers/survey';
+
+const mongoService = getMongoService();
+const notificationService = getNotificationService();
+const surveyService = getSurveyService();
+describe('create survey', () => {
+  beforeAll(async () => {
+    await mongoService.connect();
+  });
+
+  afterEach(async () => {
+    await mongoService.prune();
+  });
+
+  afterAll(async () => {
+    await mongoService.close();
+  });
+
+  it('should save the survey in the database with the provided data', async () => {
+    /**
+     * @note
+     * The createSurveyHandler function is invoked when the client/ FE application sends POST HTTP request to `/survey` endpoint create a new survey
+     */
+  });
+
+  it('should send a notification when the survey is created', async () => {
+    /**
+     * @todo - implement the test
+     */
+  });
+});
